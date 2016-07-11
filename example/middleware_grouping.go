ebb40a5a39fc7d7b8f994f98abc551cbaf0f2dc2
Mustafa Akin <mustafa91@gmail.com>
2016-07-11 16:54:21 +0300

    grouping example

---
+++ example/middleware_grouping.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"github.com/mustafaakin/gongular"
+	"log"
+)
+
+func Logger(c *gongular.Context) {
+	log.Println("Printed before every request")
+}
+
+func Index() string {
+	return "Hello, world"
+}
+
+func SomePath() int {
+	return 42
+}
+
+func main() {
+	r := gongular.NewRouter()
+
+	g1 := r.Group("/", Logger)
+	{
+		g1.GET("/", Index)
+		g1.GET("/answer", SomePath)
+		g2 := g1.Group("/admin")
+		{
+			g2.GET("/delete", Logger, Index)
+		}
+	}
+
+	r.ListenAndServe(":8000")
+
+	/*
+    ➜  ~ curl localhost:8000
+    "Hello, world"
+    ➜  ~ curl localhost:8000/answer
+    42
+    ➜  ~ curl localhost:8000/admin/delete
+    "Hello, world"
+	 */
+}
